e3d4c2b594edb2773bb349729e7ca20d29fbbf41
arjun666 <arjunmadikai996@gmail.com>
2023-06-17 14:14:41 +0530

    login register

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -135,3 +135,20 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+   
+}
+
+
+#SMTP configration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'scribblestock@gmail.com'
+EMAIL_HOST_PASSWORD = 'arjun@9746492799'
