07709e8b02718afdd7c8d0f576b14a5fb77b6d07
arjun666 <arjunmadikai996@gmail.com>
2023-06-27 11:55:50 +0530

    handled cart and checkout for the loged in user

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import *
+from carts.models import *
 
 # verifiaction email:
 from django.contrib.sites.shortcuts import get_current_site
@@ -14,6 +15,9 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.conf import settings
 
+
+from carts.views import _cart_id
+import requests
 # Create your views here.
 
 def register(request):
@@ -68,9 +72,55 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists =  CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # getting product variation by the cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # get the cart item from the user to acces the product variations
+                    cart_item = CartItem.objects.filter( user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id = item_id)
+                            item.quantity +=1
+                            item.user= user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)             
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'you are now logged in.')
-            return redirect('dashboard')
+            url = requests.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict([x.split("=") for x in query.split("&")])
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)  
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models 
 from store.models import *
+from accounts.models import *
 
 # Create your models here.
 
@@ -11,10 +12,11 @@ class Cart(models.Model):
         return self.cart_id
     
 
-class CartItem(models.Model):
+class CartItem(models.Model): 
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    cart    = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
